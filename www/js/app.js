200a48aa0684b0968d6de24b0d5afae1d426ff88
Kent Korgan <korgankd@dukes.jmu.edu>
2017-01-01 12:19:14 -0500

    run function in app.js that catch route change events

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,7 @@
 angular.module('starter', ['ionic', 'ngMockE2E'])
 
 .run(function($ionicPlatform) {
+
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -72,5 +73,31 @@ angular.module('starter', ['ionic', 'ngMockE2E'])
 })
 
 .run(function($httpBackend){
+  $httpBackend.whenGET('http://localhost:8100/valid')
+        .respond({message: 'This is my valid response!'});
+  $httpBackend.whenGET('http://localhost:8100/notauthenticated')
+        .respond(401, {message: "Not Authenticated"});
+  $httpBackend.whenGET('http://localhost:8100/notauthorized')
+        .respond(403, {message: "Not Authorized"});
+ 
   $httpBackend.whenGET(/templates\/\w+.*/).passThrough();
 });
+
+.run(function($rootScope, $state, AuthService, AUTH_EVENTS) {
+  $rootScope.$on('$stateChangeStart', function(event, next, nextParams, fromState) {
+    if('data' in next && 'authorizedRoles' in next.data) {
+      var authorizedRoles = next.data.authorizedRoles;
+      if(!AuthService.isAuthorized(authorizedRoles)) {
+        event.preventDefault();
+        $state.go($state.current, {}, {reload: true});
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+      }
+    }
+    if(!AuthService.isAuthenticated()) {
+      if(next.name !== 'login') {
+        event.preventDefault();
+        $state.go('login');
+      }
+    }
+  });
+})
