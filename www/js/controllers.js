1ea2cb843980104053f2a118964975e522e95d1d
Kent Korgan <korgankd@dukes.jmu.edu>
2017-01-01 10:52:59 -0500

    add App controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,28 @@
 angular.module('starter')
 
-.controller('AppCtrl', function() {})
+.controller('AppCtrl', function($scope, $state, $ionicPopup, AuthService, AUTH_EVENTS) {
+	$scope.username = AuthService.username();
+
+	$scope.$on(AUTH_EVENTS.notAuthorized, function(event) {
+		var alertPopup = $ionicPopup.alert({
+			title: 'Unauthorized!',
+			template: 'You are not allowed to access this resource.'
+		});
+	});
+
+	$scope.$on(AUTH_EVENTS.notAuthenticated, function(event) {
+		AuthService.logout();
+		$state.go('login');
+		var alertPopup = $ionicPopup.alert({
+			title: 'Session Lost!',
+			template: 'Sorry, You have to login again.'
+		});
+	});
+
+	$scope.setCurrentUsername = function(name) {
+		$scope.username = name;
+	};
+})
+
 .controller('LoginCtrl', function() {})
 .controller('DashCtrl', function() {});
